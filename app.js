fc490e9c05769a43819f78beffaaaa4ba15858ea
elsayedz <elsayed.z@aucegypt.edu>
2022-04-24 23:13:16 +0200

    Established connection with DB

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const {PrismaClient} = require('@prisma/client');
 
 const app = express();
+const db = new PrismaClient()
 
 app.use((req, res, next) => {
     let d = new Date();
@@ -10,8 +12,23 @@ app.use((req, res, next) => {
   });
 
 
-app.get('/', (req, res) => {
-  res.send('Successful response.');
+app.get('/', async (req, res) => {
+//   resp =  await getStads();  
+  res.send(resp);
 });
 
+// async function getStads(){
+//     const date = (new Date(2000,11,24))
+//     stads = db.user.create({
+//         data: {
+//             email: "elsayed.z@aucegypt.edu",
+//             username: "elsayedTest",
+//             age: 21,
+//             gender: 'M',
+//             birthdate: date,
+//             supportClub: "Manchester City"
+//         }
+//     })
+//     return stads;
+// }
 app.listen(3000, () => console.log('Example app is listening on port 3000.'));
